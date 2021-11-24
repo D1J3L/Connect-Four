84ef73d1638f56d76cd0d199e83f79f00524dbc4
Dyv Lee <d1j3l@Dyvs-MacBook-Pro.local>
2021-11-23 16:11:31 -0800

    Add more css and start js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+/*----- constants -----*/
+const lookup = {
+    "1": redPiece,
+    "-1": yellowPiece,
+    null: "",
+};
 
+const winningCombos = {
+
+}
+
+/*----- app's state (variables) -----*/
+let board, turn, winner;
+
+
+/*----- cached element references -----*/
+const message = document.querySelector('h2');
+const spaces = document.querySelector('td');
+
+
+/*----- event listeners -----*/
+document.querySelector("table").addEventListener("click", handleMove);
+document.querySelector('button').addEventListener('click', init);
+
+
+/*----- functions -----*/
+function init() {
+    board = [null, null, null, null, null, null, null,
+        null, null, null, null, null, null, null, 
+        null, null, null, null, null, null, null, 
+        null, null, null, null, null, null, null, 
+        null, null, null, null, null, null, null, 
+        null, null, null, null, null, null, null];
+    turn = 1;
+    winner = null;
+    render();
+}
+
+function handleMove(evt) {
+
+}
